c7ddef5524039c6f0f1ebb42301852f2cd5d2a39
Kavyashree A Baindoor <kavyashree.baindoor@ad.infosys.com>
2024-09-27 14:24:43 +0530

    day1

---
+++ Selenium/src/Day1.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		WebDriver driver=new ChromeDriver();
+		
+		System.out.println("hey");
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
